b5bdeca66d9e44c7e28087d0f0edfa46a969ead4
ken koontz <kenneth.koontz@gmail.com>
2015-02-18 16:26:12 -0800

    enable livereload

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 // var permalinks = require('metalsmith-permalinks');
+var watch = require('metalsmith-watch');
 var templates = require('metalsmith-templates');
 var serve = require('metalsmith-serve');
+var Handlebars = require('handlebars');
+var fs = require('fs');
+var partialsDir = __dirname + '/templates/partials/';
+
+Handlebars
+  .registerPartial('livereload', fs.readFileSync(partialsDir + 'livereload.hbt').toString());
 
 metalsmith(__dirname)
   .use(markdown())
@@ -10,6 +17,10 @@ metalsmith(__dirname)
   .use(serve({
     port: 8000,
     verbose: true
+  }))
+  .use(watch({
+    pattern : '**/*',
+    livereload: true
   }))
   .build(function (err) {
     if (err) {
